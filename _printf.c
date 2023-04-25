8c83e49a6bade20fce5af4083bcdf68be85e0be2
Amine Saber <saberamine000@gmail.com>
2023-04-25 09:12:39 -0700

    fixe errors

--- _printf.c
+++ _printf.c
@@ -1,52 +1,47 @@
-#include <stdarg.h>
 #include "main.h"
-#include "_putchar.c"
-#include "string.c"
 
 /**
  * _printf - A printf clone
  * @format: const pointer to a char - % include formats
  * Return: number of characters printed.
-*/
-
+ */
 int _printf(const char *format, ...)
 {
     int count = 0;
-
     va_list args;
     va_start(args, format);
 
     if (!format || !format[0])
         return (-1);
+
     while (*format)
     {
         if (*format == '%')
         {
             format++;
-            if (*format == 'c') 
+            if (*format == 'c')
             {
                 char c = va_arg(args, int);
                 count += _putchar(c);
-                
             }
             else if (*format == 's')
             {
                 char *str = va_arg(args, char *);
-                count += string(str);
+                count += _puts(str);
             }
             else if (*format == '%')
             {
                 _putchar('%');
                 count++;
             }
-            else 
+            else
             {
                 _putchar('%');
                 count++;
                 if (*format)
                 {
-                  _putchar(*format);   
-                  count++;
+                    _putchar(*format);
+                    count++;
                 }
             }
             format++;
@@ -58,5 +53,7 @@ int _printf(const char *format, ...)
             count++;
         }
     }
+    va_end(args);
     return (count);
 }
+
