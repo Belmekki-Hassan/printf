531eb2aea98cd90bb6da46cf70cc2bb7f315217d
Hassan Belmekki <hassanbelmekki@gmail.com>
2023-04-25 16:47:36 -0700

    update task 1

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ unsigned int ui;
 void *addr;
 len = _printf("Let's try to printf a simple sentence.\n");
 len2 = printf("Let's try to printf a simple sentence.\n");
-_printf("%b",99);
+_printf("%d\n",0);
 ui = (unsigned int)INT_MAX + 1024;
 addr = (void *)0x7ffe637541f0;
 _printf("Length:[%d, %i]\n", len, len);
