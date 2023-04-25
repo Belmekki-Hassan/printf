194281392bc9613783037339d292724d8eb6d6bb
Amine Saber <saberamine000@gmail.com>
2023-04-25 09:42:14 -0700

    fix betty code of _printf.c

--- _printf.c
+++ _printf.c
@@ -1,59 +1,51 @@
 #include "main.h"
 
 /**
- * _printf - A printf clone
- * @format: const pointer to a char - % include formats
- * Return: number of characters printed.
- */
+*_printf - A printf clone
+*@format: const pointer to a char - % include formats
+*Return: number of characters printed.
+*/
+
 int _printf(const char *format, ...)
 {
-    int count = 0;
-    va_list args;
-    va_start(args, format);
+int count = 0;
+va_list args;
 
-    if (!format || !format[0])
-        return (-1);
+va_start(args, format);
 
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            if (*format == 'c')
-            {
-                char c = va_arg(args, int);
-                count += _putchar(c);
-            }
-            else if (*format == 's')
-            {
-                char *str = va_arg(args, char *);
-                count += _puts(str);
-            }
-            else if (*format == '%')
-            {
-                _putchar('%');
-                count++;
-            }
-            else
-            {
-                _putchar('%');
-                count++;
-                if (*format)
-                {
-                    _putchar(*format);
-                    count++;
-                }
-            }
-            format++;
-        }
-        else
-        {
-            _putchar(*format);
-            format++;
-            count++;
-        }
-    }
-    va_end(args);
-    return (count);
+if (!format || !format[0])
+return (-1);
+
+while (*format)
+{
+if (*format == '%')
+{
+format++;
+if (*format == 'c')
+print_char(args, &count);
+else if (*format == 's')
+print_string(args, &count);
+else if (*format == '%')
+print_percent(&count);
+else
+{
+print_percent(&count);
+if (*format)
+{
+_putchar(*format);
+count++;
+}
+}
+format++;
+}
+else
+{
+_putchar(*format);
+format++;
+count++;
+}
 }
 
+va_end(args);
+return (count);
+}
--- main.h
+++ main.h
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void print_string(va_list args, int *count);
+void print_char(va_list args, int *count);
 int _putchar(char *c);
 int _printf(const char *format, ...);
 int _printfvtwo(const char *format, ...);
