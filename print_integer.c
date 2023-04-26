18f2be53de8287906165d1d5459ab1d34d684663
Hassan Belmekki <hassanbelmekki@gmail.com>
2023-04-26 02:05:57 -0700

    fixing printf for checkers

--- print_integer.c
+++ print_integer.c
@@ -8,38 +8,31 @@
 void print_integer(va_list args, int *count)
 {
 int num = va_arg(args, int);
-    int divisor = 1;
-    int skip_zero = 1;
-
-    if (num < 0)
-    {
-        _putchar('-');
-        num = -num;
-        *count += 1;
-    }
-
-    if (num == 0)
-    {
-        _putchar('0');
-        *count += 1;
-        return;
-    }
-
-    while (num / divisor > 0)
-        divisor *= 10;
-
-    while (divisor != 0)
-    {
-        int digit = num / divisor;
-        num %= divisor;
-        divisor /= 10;
-
-        if (digit == 0 && skip_zero)
-            continue;
-
-        skip_zero = 0;
-        _putchar(digit + '0');
-        *count += 1;
-    }
-
+int divisor = 1;
+int skip_zero = 1;
+if (num < 0)
+{
+_putchar('-');
+num = -num;
+*count += 1;
+}
+if (num == 0)
+{
+_putchar('0');
+*count += 1;
+return;
+}
+while (num / divisor > 0)
+divisor *= 10;
+while (divisor != 0)
+{
+int digit = num / divisor;
+num %= divisor;
+divisor /= 10;
+if (digit == 0 && skip_zero)
+continue;
+skip_zero = 0;
+_putchar(digit + '0');
+*count += 1;
+}
 }
